7babf48970330aa6133cbf90a2f6375eda51f122
deyjoy <joydey@gmail.com>
2018-09-24 01:53:32 -0400

    MessageService_Correction

--- src/main/java/org/joy/javabrains/messenger/service/MessageService.java
+++ src/main/java/org/joy/javabrains/messenger/service/MessageService.java
@@ -13,43 +13,42 @@ public class MessageService {
 
 	public List<Message> getAllMessages() {
 		return new ArrayList<Message>(messages.values());
-		
-		Message getMessage(long id) {
-			return message.get(id); 
-		}
-		
-		Message addMessage(Message message) {
-			message.setId(message.size() + 1);
-			messages.put(message.getId(), message); 
-			return message; 
-		}
-		
-		public Message updateMessage(Message message) {
-			if(message.getID() <= 0) {
-				return null; 
-			}
-			message.put(message.getId(), message); 
-			return message; 
-		}
-		
-		public Message removeMessage(long id) {
-			return message.remove(id); 
-		}
-
-		// Message m1 = new Message(1, "Hello Mauritius", "Ali");
-		// Message m2 = new Message(2, "Hello Java", "Farsad");
-		// Message m3 = new Message(3, "Hello Jersey", "Joy");
-		// Message m4 = new Message(4, "Hello Pychrm", "Refayet");
-		// Message m5 = new Message(5, "Hello Manager", "Shanchary");
-		//
-		// List<Message> list = new ArrayList<>();
-		// list.add(m1);
-		// list.add(m2);
-		// list.add(m3);
-		// list.add(m4);
-		// list.add(m5);
-		//
-		// return list;
 	}
 
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+
+	public Message updateMessage(Message message) {
+		if (message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+
+	public Message removeMessage(long id) {
+		return messages.remove(id);
+	}
+
+	// Message m1 = new Message(1, "Hello Mauritius", "Ali");
+	// Message m2 = new Message(2, "Hello Java", "Farsad");
+	// Message m3 = new Message(3, "Hello Jersey", "Joy");
+	// Message m4 = new Message(4, "Hello Pychrm", "Refayet");
+	// Message m5 = new Message(5, "Hello Manager", "Shanchary");
+	//
+	// List<Message> list = new ArrayList<>();
+	// list.add(m1);
+	// list.add(m2);
+	// list.add(m3);
+	// list.add(m4);
+	// list.add(m5);
+	//
+	// return list;
 }
